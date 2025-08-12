93cdb24c254617107f6bce640797d3313117ad00
Daniel Porrey <porrey@users.noreply.github.com>
2025-08-12 05:25:55 -0500

    Updated copyright.

--- Src/Mail.dat.Abstractions/Decorators/PropertyInfoDecorator.cs
+++ Src/Mail.dat.Abstractions/Decorators/PropertyInfoDecorator.cs
@@ -1,6 +1,6 @@
 ﻿//
 // This file is part of Open Mail.dat.
-// Copyright (c) 2025 Open Mail.dat. All rights reserved.
+// Copyright (c) 2025 Daniel Porrey, Open Mail.dat. All rights reserved.
 //
 // ************************************************************************************************************************
 // License Agreement:
